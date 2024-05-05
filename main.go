f31843a9faa81ef512061a723dcce8d458bc889e
Jie Chen <jiechenmc@gmail.com>
2024-05-05 01:25:06 -0400

    creates folderA and B if it does not exist

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func check(e error) {
@@ -28,46 +29,36 @@ func copyToDestination(src, dest string) (string, error) {
 	return dest, err
 }
 
-// func main() {
-// 	files, err := os.ReadDir("./folderA")
-// 	check(err)
-
-// 	var wg sync.WaitGroup
-// 	for _, file := range files {
-// 		wg.Add(1)
-// 		go func(file os.DirEntry) {
-// 			src := "./folderA/" + file.Name()
-// 			dest := "./folderB/" + file.Name()
-// 			_, err = copyToDestination(src, dest)
-// 			if err != nil {
-// 				fmt.Printf("%s -> %s ❌\n", src, dest)
-// 			} else {
-// 				fmt.Printf("%s -> %s ✅\n", src, dest)
-// 			}
-// 			defer wg.Done()
-// 		}(file)
-// 	}
-// 	wg.Wait()
-// }
-
 func main() {
+	_, err := os.Stat("./folderA")
+
+	if err != nil {
+		os.Mkdir("folderA", 0755)
+	}
+
 	files, err := os.ReadDir("./folderA")
 	check(err)
 
-	// var wg sync.WaitGroup
-	for _, file := range files {
-		// wg.Add(1)
-		// go func(file os.DirEntry) {
-		src := "./folderA/" + file.Name()
-		dest := "./folderB/" + file.Name()
-		_, err = copyToDestination(src, dest)
-		if err != nil {
-			fmt.Printf("%s -> %s ❌\n", src, dest)
-		} else {
-			fmt.Printf("%s -> %s ✅\n", src, dest)
-		}
-		// defer wg.Done()
-		// }(file)
+	_, err = os.Stat("./folderB")
+
+	if err != nil {
+		os.Mkdir("folderB", 0755)
 	}
-	// wg.Wait()
+
+	var wg sync.WaitGroup
+	for _, file := range files {
+		wg.Add(1)
+		go func(file os.DirEntry) {
+			src := "./folderA/" + file.Name()
+			dest := "./folderB/" + file.Name()
+			_, err = copyToDestination(src, dest)
+			if err != nil {
+				fmt.Printf("%s -> %s ❌\n", src, dest)
+			} else {
+				fmt.Printf("%s -> %s ✅\n", src, dest)
+			}
+			defer wg.Done()
+		}(file)
+	}
+	wg.Wait()
 }
